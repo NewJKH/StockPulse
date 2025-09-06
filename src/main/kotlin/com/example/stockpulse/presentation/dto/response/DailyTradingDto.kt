05cdf0359c8cba8fd6f1abe4f9e7f2c0729decd3
NewJKH <newsun0805@gmail.com>
2025-09-06 15:28:43 +0900

    ref: 카멜 케이스로 변경

--- src/main/kotlin/com/example/stockpulse/presentation/dto/response/DailyTradingDto.kt
+++ src/main/kotlin/com/example/stockpulse/presentation/dto/response/DailyTradingDto.kt
@@ -1,19 +1,23 @@
 package com.example.stockpulse.presentation.dto.response
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.UpperSnakeCaseStrategy::class)
 data class DailyTradingDto(
-    val BAS_DD: String,          // 기준일자
-    val ISU_CD: String,          // 종목코드
-    val ISU_NM: String,          // 종목명
-    val MKT_NM: String,          // 시장구분
-    val SECT_TP_NM: String,      // 소속부
-    val TDD_CLSPRC: String,      // 종가
-    val CMPPREVDD_PRC: String,   // 대비
-    val FLUC_RT: String,         // 등락률
-    val TDD_OPNPRC: String,      // 시가
-    val TDD_HGPRC: String,       // 고가
-    val TDD_LWPRC: String,       // 저가
-    val ACC_TRDVOL: String,      // 거래량
-    val ACC_TRDVAL: String,      // 거래대금
-    val MKTCAP: String,          // 시가총액
-    val LIST_SHRS: String        // 상장주식수
+    val basDd: String,           // 기준일자 (BAS_DD)
+    val isuCd: String,           // 종목코드 (ISU_CD)
+    val isuNm: String,           // 종목명 (ISU_NM)
+    val mktNm: String,           // 시장구분 (MKT_NM)
+    val sectTpNm: String,        // 소속부 (SECT_TP_NM)
+    val tddClsprc: String,       // 종가 (TDD_CLSPRC)
+    val cmpprevddPrc: String,    // 대비 (CMPPREVDD_PRC)
+    val flucRt: String,          // 등락률 (FLUC_RT)
+    val tddOpnprc: String,       // 시가 (TDD_OPNPRC)
+    val tddHgprc: String,        // 고가 (TDD_HGPRC)
+    val tddLwprc: String,        // 저가 (TDD_LWPRC)
+    val accTrdvol: String,       // 거래량 (ACC_TRDVOL)
+    val accTrdval: String,       // 거래대금 (ACC_TRDVAL)
+    val mktcap: String,          // 시가총액 (MKTCAP)
+    val listShrs: String         // 상장주식수 (LIST_SHRS)
 )
--- src/main/kotlin/com/example/stockpulse/presentation/dto/response/KrxResponse.kt
+++ src/main/kotlin/com/example/stockpulse/presentation/dto/response/KrxResponse.kt
@@ -1,5 +1,9 @@
 package com.example.stockpulse.presentation.dto.response
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.UpperSnakeCaseStrategy::class)
 data class KrxResponse(
-    val OutBlock_1 : List<DailyTradingDto>
+    val outBlock1: List<DailyTradingDto>  // OUT_BLOCK_1
 )
